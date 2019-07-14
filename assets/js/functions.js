8e3e0c699aa3f4dd82c3a872a722778fc6ea74ce
jsprlprd <jsprlprd@gmail.com>
2019-07-15 00:11:13 +0800

    sync

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -63,8 +63,3 @@ $(document).ready(function(){
   }
 });
 
-
-
-('body').on('click', '[data-toggle="modal"]', function(){
-    $($(this).data("target")+' .modal-body').load($(this).data("remote"));
-});  
